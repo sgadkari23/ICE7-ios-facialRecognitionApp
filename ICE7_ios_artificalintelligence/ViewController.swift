da4c0e2665f99dbb19ad5c399e4c68d810bf0f5f
sgadkari23 <sgadkar1@centennialcollege.ca>
2021-04-04 19:01:58 -0400

    Initial Commit

---
+++ ICE7_ios_artificalintelligence/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ICE7_ios_artificalintelligence
+//
+//  Created by Supriya G on 4/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
